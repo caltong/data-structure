d4353334d24ea62ed6ca59a0b9b4673e814dfc75
caltong <caltong1027@gmail.com>
2019-09-24 16:48:38 +0800

    测试点5 负数和0通不过

--- max_subsequence_list.py
+++ max_subsequence_list.py
@@ -6,12 +6,17 @@ list_data = list(map(int, list_data))
 
 max_sum = 0
 this_sum = 0
-this_list = []
+left = 0
+temp_left = 0
+right = list_len - 1
 for i in range(list_len):
     this_sum = this_sum + list_data[i]
     if this_sum > max_sum:
         max_sum = this_sum
+        left = temp_left
+        right = i
     elif this_sum < 0:
         this_sum = 0
+        temp_left = i + 1
 
-print(max_sum, first, last)
+print(max_sum, list_data[left], list_data[right])
