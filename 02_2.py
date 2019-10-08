c0f01cd908968f0fcbf5cc01ab04e03b245d35ce
caltong <caltong1027@gmail.com>
2019-10-08 16:47:19 +0800

    02线性结构

--- 02_2.py
+++ 02_2.py
@@ -5,24 +5,34 @@ first = list(map(int, first))
 second = second.split()
 second = list(map(int, second))
 
+# todo 
+# for i in range(first[0]):
+#     for j in range(second[0]):
+#
+first.append(0)
+first.append(0)
+second.append(0)
+second.append(0)
 first_counter = 1
 second_counter = 1
-result = []
+result_add = []
 while True:
     if first[first_counter + 1] < second[second_counter + 1]:
-        result.append(second[second_counter])
-        result.append(second[second_counter + 1])
+        result_add.append(second[second_counter])
+        result_add.append(second[second_counter + 1])
         second_counter = second_counter + 2
+
     elif first[first_counter + 1] == second[second_counter + 1]:
-        result.append(first[first_counter] + second[second_counter])
-        result.append(first[first_counter + 1])
+        result_add.append(first[first_counter] + second[second_counter])
+        result_add.append(first[first_counter + 1])
         first_counter = first_counter + 2
         second_counter = second_counter + 2
+
     elif first[first_counter + 1] > second[second_counter + 1]:
-        result.append(first[first_counter])
-        result.append(first[first_counter + 1])
+        result_add.append(first[first_counter])
+        result_add.append(first[first_counter + 1])
         first_counter = first_counter + 2
 
-    if first_counter == first[0] and second_counter == second[0]:
+    if first_counter >= 2 * first[0] and second_counter >= 2 * second[0]:
         break
-print(result)
+print(result_add)
