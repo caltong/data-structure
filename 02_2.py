5cba346fafba90bf80bc3da0059bdf1d77a617c6
caltong <caltong1027@gmail.com>
2019-10-12 16:24:19 +0800

    02_2线性结构

--- 02_2.py
+++ 02_2.py
@@ -5,14 +5,26 @@ first = list(map(int, first))
 second = second.split()
 second = list(map(int, second))
 
-# todo
-# for i in range(first[0]):
-#     for j in range(second[0]):
-#
+multi_result = []
+multi_dict = {}
 for i in range(first[0]):
     for j in range(second[0]):
-        temp_result = [first[2*i+1]*second[2*j+1],first[2*i+2]*second[2*j+2]]
-        
+        temp_result = [first[2 * i + 1] * second[2 * j + 1], first[2 * i + 2] + second[2 * j + 2]]
+        multi_result.append(temp_result)
+for i in range(len(multi_result)):
+    try:
+        temp = multi_dict[multi_result[i][1]]
+        multi_dict[multi_result[i][1]] = temp + multi_result[i][0]
+    except KeyError as e:
+        multi_dict[multi_result[i][1]] = multi_result[i][0]
+index = list(multi_dict.keys())
+index.sort()
+index.reverse()
+multi_result = []
+for i in index:
+    multi_result.append(multi_dict[i])
+    multi_result.append(i)
+
 first.append(0)
 first.append(0)
 second.append(0)
@@ -39,4 +51,6 @@ while True:
 
     if first_counter >= 2 * first[0] and second_counter >= 2 * second[0]:
         break
+
+print(multi_result)
 print(result_add)
