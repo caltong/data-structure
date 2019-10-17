3b49f2bd299357c5c2fdb05b0cc6ac142cdfb8e3
caltong <caltong1027@gmail.com>
2019-10-17 15:00:31 +0800

    最大N,最后剩K-1不反转 运行超时  其他通过

--- 02_3.py
+++ 02_3.py
@@ -9,7 +9,7 @@ def get_input_line():
 first_line = get_input_line()
 first_item_address = first_line[0]
 length = first_line[1]
-reverse_key = first_line[2]
+K = first_line[2]
 
 # 用字典存储信息
 d = {}
@@ -17,15 +17,14 @@ for i in range(length):
     line = get_input_line()
     d[line[0]] = line[1:]
 # print(d)
-
-# 顺序字典
+# 顺序
 p = first_item_address
 data_dict = {}
 while p != -1:
     data_dict[p] = d[p]
     p = d[p][1]
     # print(p)
-# print('data_dict: ', data_dict, '\n' + 'reverse_key: ', reverse_key)
+# print('data_dict: ', data_dict, '\n' + 'K: ', K)
 
 # 根据要求逆序字典keys
 tmp = []
@@ -34,25 +33,16 @@ counter = 0
 for key in data_dict.keys():
     tmp.append(key)
     counter = counter + 1
-    if counter == reverse_key:
+    if counter == K:
         tmp.reverse()
         reverse_keys = reverse_keys + tmp
         tmp = []
         counter = 0
 reverse_keys = reverse_keys + tmp
 
-# 上移一位获取next
-next_address = list(map(str, reverse_keys[1:])) + ['-1']
-for i in range(len(next_address) - 1):
-    next_address[i] = next_address[i].zfill(5)
-
-# 拼接key data next
-counter = 0
-reverse_data_dict = {}
-for key in reverse_keys:
-    reverse_data_dict[key] = [data_dict[key][0], next_address[counter]]
-    counter = counter + 1
-
 # 输出
-for key in reverse_data_dict.keys():
-    print(str(key).zfill(5), reverse_data_dict[key][0], reverse_data_dict[key][1])
+counter = 1
+for key in reverse_keys[:-1]:
+    print(str(key).zfill(5), data_dict[key][0], str(reverse_keys[counter]).zfill(5))
+    counter = counter + 1
+print(str(reverse_keys[-1]).zfill(5), data_dict[reverse_keys[-1]][0], '-1')
